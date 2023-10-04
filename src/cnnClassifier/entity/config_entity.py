3d2a814cf33b6bae3e9ebf32c35374d3ac63529c
oayk23 <omeraykilic1905@gmail.com>
2023-10-04 15:58:02 +0300

    prepare callbacks added

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -18,3 +18,12 @@ class PrepareBaseModelConfig:
     params_include_top: bool
     params_weights: str
     params_classes: int
+
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass(frozen=True)
+class PrepareCallbacksConfig:
+    root_dir: Path
+    tensorboard_root_log_dir: Path
+    checkpoint_model_filepath: Path
