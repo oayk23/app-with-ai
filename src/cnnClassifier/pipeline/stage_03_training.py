9d4fc2e67429bc47ea9e6b9f28d3300aa4411de9
oayk23 <omeraykilic1905@gmail.com>
2023-10-06 17:52:03 +0300

    dvc part added

--- src/cnnClassifier/pipeline/stage_03_training.py
+++ src/cnnClassifier/pipeline/stage_03_training.py
@@ -23,3 +23,13 @@ class ModelTrainingPipeline:
         training.train(
             callback_list=callback_list
         )
+if __name__ == '__main__':
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
